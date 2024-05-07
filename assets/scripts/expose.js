0b4a8012c5b348fa9a35fdbf52fb6fffc8e9c554
gprabhak123 <gprabhak@ucsd.edu>
2024-05-06 17:06:03 -0700

    init commit

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,16 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function selectHorn(){
+  let choice = document.getElementById('horn-select');
+  const img = new Image();
+
+  if (choice == 'air-horn'){
+    img.src = "assets/images/air-horn.svg"
+  }
+  
+}
+
 function init() {
-  // TODO
+  selectHorn();
 }
