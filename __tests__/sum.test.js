fff2c240da5e2b3846f686fb727df73f52ed0c92
gprabhak123 <gprabhak@ucsd.edu>
2024-05-07 19:53:24 -0700

    error in sum test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,9 +3,9 @@ import { sum } from '../code-to-unit-test/sum';
 
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+  expect(1 + 2).toBe(2);
 });
 
 test('adds 5 + 6 to equal 11', () =>{
-  expect(sum(5,6)).toBe(15)
+  expect(sum(5,6)).toBe(11)
 })
